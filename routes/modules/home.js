6e2c662d286563354356ef44b18deacf788ec58f
Aither <cheese736@hotmail.com>
2022-11-15 22:11:55 +0800

    modify dropdown option in homepage

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category')
 // const Resto = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  res.render('index')
-  // const userId = req.user._id
-  // Resto.find({ userId })
-  //   .lean()
-  //   .then(restaurants => res.render('index', { restaurants }))
-  //   .catch(error => console.log(error))
+  Category.find().lean()
+  .then(categories => {
+    res.render('index', {categories})
+  })
 })
 
 module.exports = router
