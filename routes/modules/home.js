8395efde7158e289b0da3d6684f461af7995c0b6
Aither <cheese736@hotmail.com>
2022-11-16 00:25:29 +0800

    trying to render homepage dynamically

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const router = express.Router()
 const Category = require('../../models/category')
-// const Resto = require('../../models/restaurant')
+const Record = require('../../models/record') 
+
+const CATEGORY = {
+  家居物業: "https://fontawesome.com/icons/home?style=solid",
+  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
+  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
+  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
+  其他: "https://fontawesome.com/icons/pen?style=solid"
+}
+console.log(CATEGORY.家居物業)
 
 router.get('/', (req, res) => {
-  Category.find().lean()
-  .then(categories => {
-    res.render('index', {categories})
-  })
+
+  async function renderHome() {
+    const userId = req.user._id
+    const categories = await Category.find().lean().sort({_id: 'asc'})
+    console.log(categories)
+    const records = await Record.find({userId}).lean()
+    const modifiedRecords = records.map(record => {
+      record.date = record.date.toISOString().split('T')[0]
+      return record
+    })
+    console.log(records)
+    res.render('index',{categories, modifiedRecords})
+  }
+  renderHome()
+  // Category.find().lean()
+  // .then(categories => {
+  //   res.render('index', {categories})
+  // })
 })
 
 module.exports = router
