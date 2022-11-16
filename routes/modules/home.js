b04e18da8e55194fe4e8d52f51a2efacabcb7e32
Aither <cheese736@hotmail.com>
2022-11-16 15:43:38 +0800

    feat: introduce fas icon

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,17 +3,28 @@ const router = express.Router()
 const Category = require('../../models/category')
 const Record = require('../../models/record') 
 
-const CATEGORY = {
-  家居物業: "https://fontawesome.com/icons/home?style=solid",
-  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
-  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
-  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
-  其他: "https://fontawesome.com/icons/pen?style=solid"
-}
-console.log(CATEGORY.家居物業)
+const images = [
+  {'fascode': '<i class="fa-solid fa-house"></i>', 'categoryId': 1}, //家居物業
+  {'fascode': '<i class="fa-solid fa-face-grin-beam"></i>', 'categoryId': 2}, //休閒
+  {'fascode': '<i class="fa-solid fa-van-shuttle"></i>', 'categoryId': 3}, //交通出行
+  {'fascode': '<i class="fa-solid fa-pen"></i>', 'categoryId': 4}, // 其他
+  {'fascode': '<i class="fa-solid fa-utensils"></i>', 'categoryId': 5}, // 餐飲食品
+]
+
+console.log(images[0].fascode)
+
+
+
+// home: "https://fontawesome.com/icons/home?style=solid",
+//   traffic: "https://fontawesome.com/icons/shuttle-van?style=solid",
+//   entertainment: "https://fontawesome.com/icons/grin-beam?style=solid",
+//   food: "https://fontawesome.com/icons/utensils?style=solid",
+//   others: "https://fontawesome.com/icons/pen?style=solid"
+
 
 router.get('/', (req, res) => {
 
+  
   async function renderHome() {
     const userId = req.user._id
     const categories = await Category.find().lean().sort({_id: 'asc'})
@@ -21,16 +32,13 @@ router.get('/', (req, res) => {
     const records = await Record.find({userId}).lean()
     const modifiedRecords = records.map(record => {
       record.date = record.date.toISOString().split('T')[0]
+      record.fascode = images.find(img => img.categoryId === record.categoryId).fascode
       return record
     })
     console.log(records)
     res.render('index',{categories, modifiedRecords})
   }
   renderHome()
-  // Category.find().lean()
-  // .then(categories => {
-  //   res.render('index', {categories})
-  // })
 })
 
 module.exports = router
