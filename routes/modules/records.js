2bda6db3eaece9d0122481a1f768bd9c442c88e6
Aither <cheese736@hotmail.com>
2022-11-18 16:58:17 +0800

    feat: add category filter route & function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,11 +3,19 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+const images = [
+  {'fascode': '<i class="fa-solid fa-house fa-2xl"></i>', 'categoryId': 1}, //家居物業
+  {'fascode': '<i class="fa-solid fa-face-grin-beam fa-2xl"></i>', 'categoryId': 2}, //休閒
+  {'fascode': '<i class="fa-solid fa-van-shuttle fa-2xl"></i>', 'categoryId': 3}, //交通出行
+  {'fascode': '<i class="fa-solid fa-pen fa-2xl"></i>', 'categoryId': 4}, // 其他
+  {'fascode': '<i class="fa-solid fa-utensils fa-2xl"></i>', 'categoryId': 5}, // 餐飲食品
+]
+
 
 // 進入新增頁面
 router.get('/new', (req, res) => {
   // 
-  Category.find().lean()
+  Category.find().lean().sort({_id: 'asc'})
   .then(categories => {
     res.render('new', {categories})
   })
@@ -29,12 +37,6 @@ router.post('/', (req, res) => {
   })
   .then(() => res.redirect('/'))
   .catch(err => console.log(err))
-})
-
-
-// 進入篩選結果結果
-router.get('/filter/:categoryId', (req, res) => {
-
 })
 
 // 刪除一筆資料
@@ -88,6 +90,29 @@ router.put('/:id', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 
+})
+
+// 進入過濾頁面
+router.get('/filter/:categoryId', (req,res) => {
+  async function categoryFilter() {
+    try {
+      const categoryId = Number(req.params.categoryId)
+      const userId = Number(req.user._id)
+      const categories = await Category.find().lean().sort({_id: 'asc'})
+      const filteredRecords = await Record.find({userId, categoryId}).lean()
+      const modifiedRecords = filteredRecords.map(record => {
+        record.date = record.date.toISOString().split('T')[0]
+        record.fascode = images.find(img => img.categoryId === record.categoryId).fascode
+        return record
+      })
+      let totalAmount = 0
+      filteredRecords.forEach(record => {totalAmount += record.amount})
+      res.render('index',{categories, modifiedRecords, totalAmount})
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  categoryFilter()
 })
 
 module.exports = router
